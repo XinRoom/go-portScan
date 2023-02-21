e33a54ee3c29ff2f704924bd5b6e100f7591b65c
XinRoom <32238570+XinRoom@users.noreply.github.com>
2023-02-21 21:57:59 +0800

    add: 添加输出记录到文件的能力2

---
+++ util/log.go
@@ -0,0 +1,19 @@
+package util
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+func NewLogger(filename string, std bool) *log.Logger {
+	var out io.Writer
+	outFile, _ := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if std {
+		out = io.MultiWriter(os.Stdout, outFile)
+	} else {
+		out = outFile
+	}
+	logger := log.New(out, "", 0)
+	return logger
+}
